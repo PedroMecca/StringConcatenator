4f949a24140a84f16e1d999c2eb67070bd15e165
pedro.bonelli <pebonellimecca@gmail.com>
2026-09-13 18:38:55 -0300

    feat: implement HomeScreen static UI

---
+++ app/src/main/java/br/edu/ifsp/scl/sc303500x/stringconcatenator/ui/theme/ui/HomeScreen.kt
@@ -0,0 +1,37 @@
+package com.example.stringconcatenator.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(
+    currentString: String,
+    onAddWordClick: () -> Unit,
+    onResetClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            value = currentString,
+            onValueChange = {},
+            readOnly = true,
+            label = { Text("String atual") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(onClick = onAddWordClick, modifier = Modifier.fillMaxWidth()) {
+            Text("Adicionar palavra")
+        }
+
+        OutlinedButton(onClick = onResetClick, modifier = Modifier.fillMaxWidth()) {
+            Text("Reiniciar")
+        }
+    }
+}
